4e891fba75099b9918272adeb6fb50da4a98e386
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2022-08-23 16:23:18 -0300

    first commit

---
+++ src/paginas/Sobre.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import '../assets/css/componentes/cartao.css'
+
+const Sobre = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Sobre J</h2>
+      </div>
+      <section className="container flex flex--centro">
+        <article className="cartao">
+          O blog do J foi criado para informar sobre o racismo no mundo!
+        </article>
+        
+      </section>
+    </main>
+  )
+}
+
+export default Sobre
